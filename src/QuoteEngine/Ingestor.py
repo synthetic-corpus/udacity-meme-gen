9b0c4f5ee31480114371eaee6cb198bd47870aec
Joel G <joel@joelgonzaga.com>
2024-06-23 11:37:52 -0700

    importing of docx now works. Ingestor.py styled.

--- src/QuoteEngine/Ingestor.py
+++ src/QuoteEngine/Ingestor.py
@@ -1,5 +1,6 @@
 """This File Contains a mod classes used to ingest quotes."""
 import pandas
+from docx import Document
 from abc import ABC, abstractmethod
 from QuoteModel import QuoteMode
 
@@ -12,7 +13,7 @@ class IngestorInterface(ABC):
     def check_extention(cls, path: str) -> None:
         """Checks the file extension for a match."""
         extension = path.split('.')[-1]
-        if(extension not in cls.extenstions):
+        if (extension not in cls.extenstions):
             raise Exception(f'Cannot import ${path} as ',
                             'one of these extentions!',
                             f'{cls.extenstions}')
@@ -34,24 +35,33 @@ class IngestCSV(IngestorInterface):
     def ingest(cls, path: str) -> list[QuoteMode]:
         """Ingests quotes from file to a list."""
         cls.check_extention(path)
-        dataframe = pandas.read_csv(path,header=0)
+        dataframe = pandas.read_csv(path, header=0)
         wise_quotes = []  # to be returned
-        for _,row in dataframe.iterrows():
+        for _, row in dataframe.iterrows():
             wise_quotes.append(
-                QuoteMode(row['body'],row['author'])
+                QuoteMode(row['body'], row['author'])
             )
         return wise_quotes
-
-
 
 
 class IngestDOCX(IngestorInterface):
     """Ingest the docx format."""
     extenstions = ['docx']
 
+    @classmethod
     def ingest(cls, path: str) -> list[QuoteMode]:
-        """Each Realization will override."""
-        pass
+        """Iterate over each docx line and create Quotes."""
+        cls.check_extention(path)
+        doc = Document(path)
+        wise_quotes = []
+        for line in doc.paragraphs:
+            if len(line.text) > 0:  # Sometimes line are blank.
+                string = line.text.replace('"', '')
+                array = string.split("-")
+                wise_quotes.append(
+                    QuoteMode(array[0], array[1])
+                )
+        return wise_quotes
 
 
 class IngestPDF(IngestorInterface):
