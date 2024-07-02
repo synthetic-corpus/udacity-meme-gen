d776ba7f303eaa3f946b90a32fea3a254751a146
Joel G <joel@joelgonzaga.com>
2024-07-01 21:11:25 -0700

    Web Engine Linted

--- src/WebEngine/Initializer.py
+++ src/WebEngine/Initializer.py
@@ -1,4 +1,4 @@
-""" This function is repsonsible for downloading 
+""" This function is repsonsible for downloading
 content from a public s3 bucket."""
 import pandas
 from WebRequester import ImageRequestor, TextRequestor
@@ -6,13 +6,15 @@ from WebRequester import ImageRequestor, TextRequestor
 image_requests = ImageRequestor('../_data/photos')
 text_request = TextRequestor('../_data/SimpleLines')
 
+
 def init_from_s3():
-    data_frame = pandas.read_csv('./init.csv',header=0)
+    data_frame = pandas.read_csv('./init.csv', header=0)
     for _, row in data_frame.iterrows():
         print(f'importing: {row['url']}')
         if row['type'] == 'img':
             image_requests.get_file(row['url'])
         else:
             text_request.get_file(row['url'])
+
 
 init_from_s3()
