6a21a692ce618c96200d6366fb6304bcce4c8cc8
Joel G <joel@joelgonzaga.com>
2024-07-01 21:05:16 -0700

    setup.py linted.

--- src/setup.py
+++ src/setup.py
@@ -5,6 +5,7 @@
 import os
 from QuoteEngine import Ingestor
 
+
 def get_files(path: str, *extensions: str) -> list[str]:
     """Get all files from path that
         match specified exentions."""
@@ -18,16 +19,15 @@ def get_files(path: str, *extensions: str) -> list[str]:
     return all_files
 
 
-
 def setup():
     """ Load all resources """
 
-    quote_files = get_files('./_data','txt','docx','csv','pdf')
+    quote_files = get_files('./_data', 'txt', 'docx', 'csv', 'pdf')
     print(quote_files)
     all_quotes = []
     for file_path in quote_files:
         try:
-            more_quotes =  Ingestor.parse(file_path)
+            more_quotes = Ingestor.parse(file_path)
         except TypeError as e:
             print(
                 (f'Invalid file type ${file_path}'),
@@ -37,6 +37,6 @@ def setup():
             all_quotes.extend(more_quotes)
 
     image_path = './_data'
-    imgs = get_files(image_path,'jpg','jpeg','png')
+    imgs = get_files(image_path, 'jpg', 'jpeg', 'png')
 
     return all_quotes, imgs
