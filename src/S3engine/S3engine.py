1de7e50861c4f2d0235343cac8c6512f88f42e86
Joel G <joel@joelgonzaga.com>
2024-12-29 14:36:27 -0800

    doing path join without seperators.

--- src/S3engine/S3engine.py
+++ src/S3engine/S3engine.py
@@ -93,12 +93,13 @@ class S3engine:
     def load_quotes(self):
         """ Loads quotes into a local folder on ec2 """
         quotes = self.list_content('_textdata')
-        relative_path = '/../_data/miniquotes/'
+        relative_path = '../_data/miniquotes'
         sources = []
         for quote_tuple in quotes:
             quote_data, quote_name = self.get_file(quote_tuple[0])
             relative_path = f'{relative_path}{quote_name}'
-            save_here = os.path.join(os.path.dirname(__file__), relative_path)
+            save_here = os.path.join(os.path.dirname(__file__),
+                                     relative_path)
             try:
                 with open(save_here, 'wb') as f:
                     f.write(quote_data.read())
